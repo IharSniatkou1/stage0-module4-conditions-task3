4db0da41d9c0bb101aa3831924eabdc215652eda
Ihar Sniatkou <iharsniatkou@gmail.com>
2024-11-01 15:17:37 +0100

    Stage0-module4-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) {
+
+            char lowercaseChar = Character.toLowerCase(character);
+
+            if (lowercaseChar == 'a' || lowercaseChar == 'e' || lowercaseChar == 'i' ||
+                    lowercaseChar == 'o' || lowercaseChar == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
